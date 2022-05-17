1970943658bd040407f2bbb29129df57d9ebfe3c
Advik Kaul <105526703+blazeak08@users.noreply.github.com>
2022-05-17 19:12:57 -0400

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,14 @@
+print("Hello! Welcome to my very first madlib!!")
+
+animal = input("Enter the most scary animal you know: ")
+food  = input("What's your favorite food: ")
+name = input("Enter your best friend's name: ").title()
+vehicle = input("Enter the name of your favorite vehicle: ")
+liquid = input("Enter your favorite drink: ")
+
+print(f"""\nOne day, a {animal} was eating {food} when {name} picked it up and thought 
+it was so cute. {name} decdied to adopt it. The {animal} was not very happy being a pet, 
+so he climbed into his owners toy {vehicle} to run off. In the {vehicle}, the {animal} found 
+a bottle of {liquid}. When the {animal} drank it, he got a huge adrenaline rush and 
+drove the {vehicle} out of the window!!! Luckily, it was not a big drop from the window so the 
+{animal} didn’t get hurt. He drove off in the {vehicle} into the big world again. """)
